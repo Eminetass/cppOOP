3df8c488712c9c8d2c9a2defb45c8ab5355bc1a7
Emine Tas <eminetas126@gmail.com>
2024-03-11 15:10:19 +0300

    calisma

---
+++ C++OOP/CppOOP13.cpp
@@ -0,0 +1,17 @@
+/*#include <iostream>
+#include "Kolon.h"
+#include <time.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+    srand(time(NULL));
+	Kolon k(5);
+	for(int i=0;i<5;i++)
+	   k.set(i,rand() % 50);
+	k.print();
+	Kolon k2;
+	k2 = k;
+	k2.print();
+	
+	return 0;
+}*/
